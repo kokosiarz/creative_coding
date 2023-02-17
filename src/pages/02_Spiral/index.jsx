4ca83c84a3db9c77869b11fe74b4bc1bb5bc5822
Krzysztof Kokosiński <kokosiarz@gmail.com>
2023-02-18 00:12:07 +0100

    add sound and textfield

--- src/pages/02_Spiral/index.jsx
+++ src/pages/02_Spiral/index.jsx
@@ -3,14 +3,16 @@ import './styles.css';
 import spiral from '../../p5js/05a_spiral-SOS-counter/sketch.js'
 import P5Wrap from '../../components/p5wrap';
 import TypeOut from 'react-typeout';
+import sound from "./acna_malas.mp3";
 
 const Spiral = () => {
   const words = ["         ", "Miles to go", "before I sleep", "you've let me down", "no reason for me to stay here"];
-
   return (
     <>
       <TypeOut words={words} />
       <P5Wrap fn={spiral}/>
+      <input type="text" name="name" />
+      <audio src={sound} autoPlay loop/>
     </>
   );
 }
